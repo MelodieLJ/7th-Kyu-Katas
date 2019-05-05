2bbfc16a245fcdba3c2b354ea59f34deac2b96ec
melodie <josmelodie@gmail.com>
2019-05-05 23:21:59 +1200

    Challenge when provided with a String, capitalize all vowels

---
+++ ChangingVowels/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangingVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Swap("HelloWorld!"));
+        }
+    }
+
+    public static class Kata
+    {
+        public static string Swap(string s)
+        {
+            string vowels = "aeiou";
+            string result = "";
+
+            foreach (var item in s)
+            {
+                if (vowels.Contains(item))
+                {
+                    result += char.ToUpper(item);
+                }
+                else
+                {
+                    result += item;
+                }
+            }
+            return result;                     
+        }
+    }
+}
