91ebc861e89697156753fef8347da3b158773929
melodie <josmelodie@gmail.com>
2019-05-05 21:58:12 +1200

    Add project files.

---
+++ 7th KYU/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7th_KYU
+{
+    /*Return the number (count) of vowels in the given string.
+    We will consider a, e, i, o, and u as vowels for this Kata.
+    The input string will only consist of lower case letters and/or spaces.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetVowelCount("Melodie"));
+        }
+    }
+
+    public static class Kata
+    {
+        public static int GetVowelCount(string str)
+        {
+            string word = str.ToLower();
+            string vowel = "aeiou";
+            int count = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (vowel.Contains(word[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
