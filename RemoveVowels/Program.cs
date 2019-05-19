12046ef016f5914902350b8291fc76124ce22cb3
melodie <josmelodie@gmail.com>
2019-05-19 22:59:40 +1200

    Challenge: To create a function that removes all vowels from a string

---
+++ RemoveVowels/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveVowels
+{
+    class Program
+    {
+        //Challenge: Remove all vowels from a string
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Diwemvowel("No more vowels"));
+        }
+    }
+
+    public static class Kata
+    {
+        public static string Diwemvowel(string str)
+        {
+            string vowels = "aeiouAEIOU";
+            char[] temp = str.ToArray();
+            List<char> result = new List<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!vowels.Contains(str[i]))
+                {
+                    result.Add(str[i]);
+                }
+            }
+
+            return string.Join("", result);
+        }
+    }
+}
