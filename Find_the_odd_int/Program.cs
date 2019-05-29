9cc0d124005d5b3227af35cba5c79637ee0b2dbe
melodie <josmelodie@gmail.com>
2019-05-29 22:39:43 +1200

    Challenge: Find the total sum of angles in an n sided shape. N will be greater than 2

---
+++ Find_the_odd_int/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_odd_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Find_it(new int[] { 20} ));
+        }
+    }
+
+    public static class Kata
+    {
+        public static int Find_it(int[] seq)
+        {
+            //int count = 0;
+
+            //for (int i = 0; i < seq.Length; i++)
+            //{
+            //    if (seq[i] % 2 != 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //return count;
+
+            for (int i = 0; i < seq.Length; i++)
+            {
+                for (int j = 0; j < seq.Length; j++)
+                {
+
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Sum_of_Angles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Angles
+{
+    //Challenge: Find the total sum of angles in an n sided shape. N will be greater than 2
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Angle(5));
+        }
+    }
+
+    public class Kata
+    {
+        public static int Angle(int n)
+        {
+            return 180 * (n - 2);
+        }
+    }
+}
