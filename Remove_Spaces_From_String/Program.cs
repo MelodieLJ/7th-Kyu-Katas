28f382fdc240cf37a2d85f650caba6df38f32a89
melodie <josmelodie@gmail.com>
2019-05-29 22:02:06 +1200

    Challenge: To remove spaces from a string

---
+++ Remove_Spaces_From_String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Spaces_From_String
+{
+    class Program
+    {
+        //Challenge: Remove spaces form a string
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.NoSpace("Coding is great !"));
+        }
+    }
+    public static class Kata
+    {
+        public static string NoSpace(string input)
+        {
+            return input.Replace(" ", "");
+        }
+    }
+}
