5e7a22bdfd940418d24c1aae5fc202ce5faeae30
melodie <josmelodie@gmail.com>
2019-05-29 21:53:33 +1200

    Challenge: To define a function that takes an integer argument and returns logical value true or false depending on if the integer is a prime.

---
+++ CheckForPrimeNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckForPrimeNumbers
+{
+    // Challenge: Define a function that takes an integer argument and returns logical value true or false depending on if the integer is a prime.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join("", Kata.IsPrime(19)));
+        }
+    }
+
+    public static class Kata
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            if (n == 2)
+                return true;
+            if (n % 2 == 0)
+                return false;
+
+            var x = (int)Math.Floor(Math.Sqrt(n));
+
+            for (int i = 3; i <= x; i+=2)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
--- Tuples_OrderedCount/Program.cs
+++ Tuples_OrderedCount/Program.cs
@@ -10,7 +10,7 @@ namespace Tuples_OrderedCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(string.Join(", \n", Kata.OrderedCount("dayoneofonehundreddaysofcode")));
+            Console.WriteLine(string.Join(", \n", Kata.OrderedCount("daythreeofonehundreddaysofcode")));
         }
     }
 
@@ -18,18 +18,26 @@ namespace Tuples_OrderedCount
     {
         public static List<Tuple<char, int>> OrderedCount(string input)
         {
+            //Create a Tuple list to store the result
             List<Tuple<char, int>> result = new List<Tuple<char, int>>();
+
+            //Create a do/while loop whilst the string length is greater than 0
             do
             {
+                //assign char variable to the first letter in the string argument
                 char letter = input[0];
                 int count = 0;
 
+                //iterate through all the letters in the string
                 for (int i = 0; i < input.Length; i++)
                 {
+                    
                     if (input[i] == letter)
                     {
+                        //if the letter at index of i is equal to the value stored in letter, increase the count and remove that letter from the string
                         count++;
                         input = input.Remove(i, 1);
+                        //i-- is to ensure that the check is always done for the first letter in the string
                         i--;
                     }
                 }
@@ -40,4 +48,3 @@ namespace Tuples_OrderedCount
         }
     }
 }
-
