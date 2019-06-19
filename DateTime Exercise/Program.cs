946c231432dfe85f81384857cca95d87cbdf1d14
melodie <josmelodie@gmail.com>
2019-06-19 21:19:43 +1200

    Challenge: Given an int, return a square that is as long and as wide as the int

--- DateTime Exercise/Program.cs
+++ DateTime Exercise/Program.cs
@@ -1,19 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DateTime_Exercise
 {
-    /* Challenge: Find the number of Friday the 13th's in the given year. Input: Year as an integer. Output: Number of Black Fridays in the year as an integer.
+    /*Challenge: Find the number of Friday the 13th's in the given year. Input: Year as an integer. Output: Number of Black Fridays in the year as an integer.
      */
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Kata.UnluckyDays(2819));
+            var year = 2019;
+            var result = Kata.UnluckyDays(year);
+
+            Console.WriteLine($"Number of Unlucky days in the year {year}: {result}");
         }
     }
 
@@ -22,8 +21,8 @@ namespace DateTime_Exercise
         public static int UnluckyDays(int year)
         {
             int count = 0;
-
-            for (int i = 1; i < 13; i++)
+            //loop through the months
+            for (int i = 1; i <= 12; i++)
             {
                 DateTime date = new DateTime(year, i, 13);
                 if (date.DayOfWeek == DayOfWeek.Friday)
---
+++ Generate Square/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate_Square
+{
+    // Challenge: Given an int, return a square that is as long and as wide as the int
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GenerateSquare(3));
+        }
+    }
+
+    public static class Kata
+    {
+        public static string GenerateSquare(int n)
+        {
+            //create a string array to store the square
+            string[] result = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result[i] += "+";
+                }
+            }
+            //separate each row by a space
+            return string.Join("\n", result);
+        }
+    }
+}
