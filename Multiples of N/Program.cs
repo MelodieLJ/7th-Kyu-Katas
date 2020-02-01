881f3ca8af45789f9f20407e24c13b6c773c37fc
Melodie Joseph <josmelodie@gmail.com>
2020-02-01 20:42:17 +1300

    Challenge: Write a function that returns true if the number is a "Very Even" number.

---
+++ Multiples of N/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Multiples_of_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Multiples(3, 5));
+        }
+    }
+
+    public static class Kata
+    {
+        public static double[] Multiples(int m, double n)
+        {
+            List<double> temp = new List<double>();
+
+            for (int i = 1; i < m; i++)
+            {
+                temp.Add(n * i);
+            }
+            return temp.ToArray();
+        }
+    }
+}
---
+++ Very Even Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Very_Even_Numbers
+{
+    //Challenge: Write a function that returns true if the number is a "Very Even" number. For example:
+    //input(88) => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.IsVeryEvenNumber(88));
+        }
+    }
+
+    public static class Kata
+    {
+        public static bool IsVeryEvenNumber(int number)
+        {
+            //to reduce number to single digit
+            while (number >= 10)
+            {
+                number = number % 10 + number / 10;
+            }
+            return number % 2 == 0;
+        }
+    }
+}
